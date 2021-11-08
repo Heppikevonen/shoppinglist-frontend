70534eeb09d80d12ddaba56cb1e4c39f2034635b
Heppikevonen <hegezu@windowslive.com>
2021-11-08 19:22:51 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
-function App() {
+const URL = 'http://localhost/shoppinglist/'
+
+function App () {
+  const [items, setItems] = useState([])
+  const [item, setItem] = useState('')
+  const [amount, setAmount] = useState('')
+
+  function add (e) {
+    e.preventDefault()
+    const json = JSON.stringify({ description: item, amount: amount })
+    axios
+      .post(URL + 'add.php', json, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => {
+        console.log(response.data)
+        setItems(items => [...items, response.data])
+      })
+      .catch(error => {
+        alert(error.response.data.error)
+      })
+  }
+
+  function remove (id) {
+    const json = JSON.stringify({ id: id })
+    axios
+      .post(URL + 'delete.php', json, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => {
+        console.log(response.data)
+        const newListWithoutRemoved = items.filter(item => item.id !== id)
+        setItems(newListWithoutRemoved)
+      })
+      .catch(error => {
+        alert(error.response ? error.response.data.error : error)
+      })
+  }
+
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then(response => {
+        console.log(response.data)
+        setItems(response.data)
+      })
+      .catch(error => {
+        alert(error.response ? error.response.data.error : error)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h3>Shopping list </h3>
+      <form onSubmit={add}>
+        <label>New item</label>
+        <input
+          value={item}
+          placeholder='type description'
+          onChange={e => setItem(e.target.value)}
+        />
+        <input
+          value={amount}
+          placeholder='type amount'
+          type='number'
+          onChange={e => setAmount(e.target.value)}
+        />
+        <button>Add</button>
+      </form>
+      <table>
+        {items?.map(item => (
+          <tr key={item.id}>
+            <td><span>{item.description}</span></td>
+            <td><span>{item.amount}</span></td>
+            <a href='#' className='delete' onClick={() => remove(item.id)}>
+              Delete
+            </a>
+            &nbsp;
+          </tr>
+        ))}
+      </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
